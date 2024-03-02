eb497a57250d0a899dfce31f63a43346e4ab4696
fearalert <dhakalrohan229@gmail.com>
2024-03-02 16:33:44 +0545

    updated projects and contact forms

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,21 +1,22 @@
 import { CodeIcon } from "@heroicons/react/solid";
 import React from "react";
-import { projects } from "./data/Data";
+import { designs, projects } from "./data/Data";
 
 
 export default function Projects() {
   return (
-    <section id="projects" className="text-gray-400 bg-gray-900 body-font">
+    <section id="projects" className="text-white-gray bg-background-black body-font">
       <div className="container px-5 py-10 mx-auto text-center lg:px-40">
         <div className="flex flex-col w-full mb-20">
           <CodeIcon className="mx-auto inline-block w-10 mb-4" />
-          <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-white">
+          <h1 className="sm:text-4xl text-4xl font-medium title-font mb-4 text-white">
             My Projects
           </h1>
           <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
-            Here, are the list of my programming as well as design projects.
+            Here, are the list of my programming and design works.
           </p>
         </div>
+        
         <div className="flex flex-wrap -m-4">
           {projects.map((project) => (
             <a
@@ -28,11 +29,11 @@ export default function Projects() {
                   className="absolute inset-0 w-full h-full object-cover object-center"
                   src={project.image}
                 />
-                <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
-                  <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
+                <div className="px-8 py-10 relative z-10 w-full border-4 border-dodger-blue bg-button-gray opacity-0 hover:opacity-100">
+                  <h2 className="tracking-widest text-sm title-font font-medium text-dodger-blue mb-1">
                     {project.subtitle}
                   </h2>
-                  <h1 className="title-font text-lg font-medium text-white mb-3">
+                  <h1 className="title-font text-lg font-medium text-lg text-white mb-3">
                     {project.title}
                   </h1>
                   <p className="leading-relaxed">{project.description}</p>
@@ -42,6 +43,7 @@ export default function Projects() {
           ))}
         </div>
       </div>
+{/* Design Projects */}
     </section>
   );
 }
--- src/components/data/Data.js
+++ src/components/data/Data.js
@@ -3,9 +3,9 @@ export const projects = [
       title: "Personal Portfolio",
       subtitle: "Personal Project",
       description:
-        "This project is a personal portfolio website made using React.js, CSS and JavaScript. This is a personal Project.",
-      image: "https://purepng.com/public/uploads/large/purepng.com-rubiks-cube3-d-combination-puzzlerubikcubeclassicclipartpuzzle-1421526591425jgi2x.png",
-      link: "https://github.com/fearalert/computer-graphics",
+        "This project is a personal portfolio website made using React.js, tailwindCSS and JavaScript.",
+      image: "https://th.bing.com/th/id/OIP.8jN0KAPEam88i3ntYEY7DwHaE5?rs=1&pid=ImgDetMain",
+      link: "https://github.com/fearalert/personal_website",
     },
     {
       title: "Rubik's Cube 3D Visualization",
@@ -20,7 +20,7 @@ export const projects = [
       subtitle: "Semester Project",
       description:
         " A travel guide android application for Nepal with integrated language translation using Google API.",
-      image: "https://th.bing.com/th/id/OIP.Eit6eCZtAjyVvorBeQpt_QHaFj?w=232&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+      image: "https://th.bing.com/th/id/OIP.WIDARL3wURtF_ojJD2Jn2gHaEo?rs=1&pid=ImgDetMain",
       link: "https://github.com/fearalert/TravelWay",
     },
     {
@@ -36,7 +36,7 @@ export const projects = [
       subtitle: "Semester Project",
       description:
       "An android application to bridge the gap between normal people anddaily life technicians. E.g. Plumber, Mechanic, etc.",
-      image: "https://th.bing.com/th/id/OIP.fGkzKrChmflU2_QlgRY60gHaE8?rs=1&pid=ImgDetMain",
+      image: "https://handymantips.org/wp-content/uploads/2020/03/Getting-The-Utilities-Set-Up-For-Your-New-Home-In-Houston-600x571.jpg",
       link: "https://github.com/fearalert/Home-Utility",
     },
     {
@@ -44,7 +44,7 @@ export const projects = [
       subtitle: "Semester Project",
       description:
       "A download manager for linux based systems using aria2 library, c++, and Qt.",
-      image: "https://th.bing.com/th/id/OIP.fGkzKrChmflU2_QlgRY60gHaE8?rs=1&pid=ImgDetMain",
+      image: "https://th.bing.com/th/id/OIP.Zo4Hxhf3E1qgSdGzuHzoTAHaHa?rs=1&pid=ImgDetMain",
       link: "https://github.com/fearalert/KULOADER-",
     },
 
@@ -54,18 +54,41 @@ export const projects = [
       description:
         " A travel guide android application for Nepal with integrated language translation using Google API.",
       image: "https://th.bing.com/th/id/OIP.Eit6eCZtAjyVvorBeQpt_QHaFj?w=232&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
-      link: "https://github.com/fearalert/TravelWay",
+      link: "https://www.behance.net/gallery/192838163/Travelway",
+    },
+    {
+      title: "KUCC CUP 2023",
+      subtitle: "Poster Designs",
+      description:
+      "KUCC CUP 2023 posters, banners and certificates designs. Done as graphics designer for KUCC in 2023.",
+      image: "https://th.bing.com/th/id/OIP.-4Ium6x51cbN5B1ca7J1egHaGB?w=197&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+      link: "https://www.behance.net/gallery/192846515/Poster-Design-KUCC-CUP-2023-(Kathmandu-University)",
+    },
+    {
+      title: "E-prescribe",
+      subtitle: "UI/UX Design",
+      description:
+      "This is an UI design for the E-prescribe android/IOS application. It includes registration and login screens.",
+      image: "https://th.bing.com/th/id/OIP.-aX8wZ6edVZpFPJ7zIH9_QAAAA?rs=1&pid=ImgDetMain",
+      link: "https://www.behance.net/gallery/192834963/E-Prescribe-APP-MOCKUP",
     },
     {
       title: "Medizine",
-      subtitle: "Semester Project",
+      subtitle: "UI/UX Design",
       description:
-      "A doctor appointment Android application made using Flutter and Node.js with HL7 FHIR standards.",
-      image: "https://th.bing.com/th/id/OIP.fGkzKrChmflU2_QlgRY60gHaE8?rs=1&pid=ImgDetMain",
-      link: "https://github.com/fearalert/Medizine",
+      "This is an UI design for the online doctor appointment booking system. This application helps to manage and schedule the appointments.",
+      image: "https://th.bing.com/th/id/OIP.kJlaY9pvFOaLfTzZbGbX9AHaD4?rs=1&pid=ImgDetMain",
+      link: "https://www.behance.net/gallery/192836151/Medizine-Online-Doctor-Appointment-App",
     },
   ];
 
+
+
+
+
+
+
+  
 export const skills = [
  'HTML', 'CSS', 'UI/UX Design', 'Graphic Design', 'C/C++', 'Python', 'Web Design',
  'Research', 'ReactJs', 'Documentation', 'MS Package (Word, Excel, Powerpoint, Latex)', 
